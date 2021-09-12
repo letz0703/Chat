293e3a62f9249e120fe58877ec514c944aeb8467
letz <letz0703@gmail.com>
2021-09-12 19:34:48 +0900

    Password Reset
    new activity ForgotActivity
    1. design activity_forgot.xml
    2. define components

---
+++ app/src/main/java/com/letz/chat/ForgotActivity.java
@@ -0,0 +1,26 @@
+package com.letz.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ForgotActivity extends AppCompatActivity
+{
+
+    private TextInputEditText editTextForgot;
+    private Button buttonForgot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot);
+
+        editTextForgot = findViewById(R.id.etEmail_ForgotActivity);
+        buttonForgot = findViewById(R.id.btnReset_ForgotActivity);
+
+
+    }
+}
