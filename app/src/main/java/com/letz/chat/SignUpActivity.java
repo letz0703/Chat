b5784158e063f4ba04433c284646f347cfc6ac0c
letz <letz0703@gmail.com>
2021-09-14 22:52:37 +0900

    Sign up
    	1. onClick for iv and btn
    	• access a photo in the device
    	public void imageChooser()
    
    	2. picasso library to build.gradle(:app)
    
    	3. check image loading
    	boolean imageControl=false;
    	imageControl=true;
    
    	4. ivCircle 누르면 imageChooser
    	5. btnRegister
    	email, password, userName 받고 signup
    	• new method signup
    	• define:
    	auth, database, reference
    	• signup()
    6 .child(auth.getUid()???
    	• if (imagecontrol)이면{}
    else reference.child("users"
    .child(auth.getUid()).child("image")
    .setValue("null")
    	7. Back to Main

--- app/src/main/java/com/letz/chat/SignUpActivity.java
+++ app/src/main/java/com/letz/chat/SignUpActivity.java
@@ -1,30 +1,139 @@
 package com.letz.chat;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignUpActivity extends AppCompatActivity
 {
-    private CircleImageView imageViewCircle;
-    private TextInputEditText email, password, username;
+    private CircleImageView ivCircle;
+    private TextInputEditText email, password, name;
     private Button register;
+    boolean imageControl = false;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        imageViewCircle = findViewById(R.id.ivCircle);
+        ivCircle = findViewById(R.id.ivCircle);
         email = findViewById(R.id.etEmailSignUp);
         password = findViewById(R.id.etPasswordSignUp);
-        username = findViewById(R.id.etUserNameSignUp);
+        name = findViewById(R.id.etUserNameSignUp);
         register = findViewById(R.id.btnRegister);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        ivCircle.setOnClickListener(v -> {
+            imageChooser();
+        });
+
+        register.setOnClickListener(v -> {
+            String userEmail = email.getText().toString();
+            String userPassword = password.getText().toString();
+            String userName = name.getText().toString();
+
+            if (!userEmail.equals("") && !userPassword.equals("") && !userName.equals("")) {
+                signup(userEmail, userPassword, userName);
+            }
+
+
+        });
+
     }
+
+    public void imageChooser() {
+        //image 열기
+        Intent i = new Intent();
+        i.setType("images/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        launchLoadImage.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchLoadImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>()
+            {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri imageUri = data.getData();
+                        Picasso.get().load(imageUri).into(ivCircle);
+                        imageControl = true;
+                    } else {
+                        imageControl = false;
+                    }
+                }
+            }
+    );
+
+    public void signup(String email, String password, String username) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            reference.child("Users")
+                                    .child(auth.getUid())
+                                    .child("userName")
+                                    .setValue(username);
+
+                            if (imageControl)//이미지 선택되었을 경우
+                            {
+
+                            } else // null 넣어준다.
+                            {
+                                reference.child("Users").child(auth.getUid()).child("image")
+                                        .setValue("null");
+                            }
+
+                            Intent igoMain = new Intent(SignUpActivity.this, MainActivity.class);
+                            igoMain.putExtra("userName", username);
+                            startActivity(igoMain);
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Failed SignUp", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
 }
+
+
+
+
+
+
