34d1365444797fadfb707c2254ae194f5b64965a
letz0703 <letz0703@gmail.com>
2021-11-03 13:36:20 +0900

    wip Model class nad MessageAdapter

---
+++ app/src/main/java/com/letz/chat/MessageAdapter.java
@@ -0,0 +1,58 @@
+package com.letz.chat;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder>
+{
+    List<ModelClass> list;
+    String userName;
+
+    boolean status;
+    int send;
+    int receive;
+
+    public MessageAdapter(List<ModelClass> list, String userName) {
+        this.list = list;
+        this.userName = userName;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView textView;
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (list.get(position).getFrom().equals(userName))
+        {
+
+        }
+        return super.getItemViewType(position);
+    }
+}
